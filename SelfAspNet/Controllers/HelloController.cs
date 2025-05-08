92508bb0a1f3a0888ab8f975d80db499ca0a826a
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-05-08 12:47:57 +0900

    add Hello Show

--- SelfAspNet/Controllers/HelloController.cs
+++ SelfAspNet/Controllers/HelloController.cs
@@ -9,4 +9,10 @@ public class HelloController : Controller
     {
         return Content("こんにちは、世界！");
     }
+
+    public IActionResult Show()
+    {
+      ViewBag.Message = "こんにちは、世界！";
+      return View();
+    }
 }
