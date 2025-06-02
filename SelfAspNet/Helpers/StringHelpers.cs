10baca79fd125b88be6f4ea84aa490edbbc861ce
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-06-02 11:48:55 +0900

    Change to extended method

--- SelfAspNet/Helpers/StringHelpers.cs
+++ SelfAspNet/Helpers/StringHelpers.cs
@@ -1,10 +1,12 @@
 using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace SelfAspNet.Helpers;
 
 public static class StringHelpers
 {
-    public static string Truncate(string text, int length = 15, string omission = "...")
+    public static string Truncate(this IHtmlHelper helper,
+        string text, int length = 15, string omission = "...")
     {
         if (text.Length <= length) { return text; }
         return text.Substring(0, length - 1) + omission;
