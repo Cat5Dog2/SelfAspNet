16c682c6f57207b8662ae56c82598d19120ffd72
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-06-18 13:45:46 +0900

    Create Review Model

---
+++ SelfAspNet/Models/Review.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace SelfAspNet.Models;
+
+public class Review
+{
+    public int Id { get; set; }
+    [Display(Name ="名前")]
+    public string Name { get; set; } = String.Empty;
+    [Display(Name ="レビュー")]
+    public string Body { get; set; } = String.Empty;
+    [Display(Name ="更新日")]
+    public DateTime LastUpdate { get; set; } = DateTime.Now;
+    [Display(Name ="書籍")]
+    public int BookId { get; set; }
+    public Book Book { get; set; } = null!;
+}
