7e0b17688b11f85a05214443077900073f86cf33
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-06-19 17:57:24 +0900

    Add book information and review display function

---
+++ SelfAspNet/Controllers/EntityController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SelfAspNet.Models;
+
+namespace SelfAspNet.Controllers;
+
+public class EntityController : Controller
+{
+    private readonly MyContext _db;
+    public EntityController(MyContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<IActionResult> Assoc(int id = 1)
+    {
+        var b = await _db.Books
+            .Include(b => b.Reviews)
+            .Include(b => b.Authors)
+            .ThenInclude(a => a.User)
+            .SingleAsync(b => b.Id == id);
+        return View(b);
+    }
+}
