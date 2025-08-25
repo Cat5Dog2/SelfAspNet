2ef1b278e33adae9bfc09c14215d8bc75d73602a
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-08-25 16:58:25 +0900

    Add validation rules to the Book entity

--- SelfAspNet/Models/Book.cs
+++ SelfAspNet/Models/Book.cs
@@ -7,21 +7,37 @@ public class Book
 {
     [Display(Name = "ID")]
     public int Id { get; set; }
+
+    [RegularExpression("978-4-[0-9]{2,5}-[0-9]{2,5}-[0-9X]",
+        ErrorMessage = "{0}の形式が誤っています。")]
     [Display(Name = "ISBN")]
     [DataType(DataType.ImageUrl)]
     public string Isbn { get; set; } = String.Empty;
+
+    [Required(ErrorMessage = "{0}は必須です。")]
+    [StringLength(50, ErrorMessage = "{0}は{1}文字以内で指定してください。")]
     [Display(Name = "タイトル")]
     public string Title { get; set; } = String.Empty;
+
+    [Range(10, 10000, ErrorMessage = "{0}は{1} ~ {2}の間で指定してください。")]
     [DataType(DataType.Currency)]
     [Display(Name = "価格")]
     public int Price { get; set; }
+
+    [RegularExpression("翔泳社|技術評論社|SBクリエイティブ|日経BP|森北出版",
+        ErrorMessage = "{0}は「{1}」のいずれかでなければなりません。")]
     [Display(Name = "出版社")]
     public string Publisher { get; set; } = String.Empty;
+
+    [Range(typeof(DateTime), "2010-01-01", "2029-12-31",
+        ErrorMessage = "{0}は{1:d} ~ {2:d}の範囲で指定してください。")]
     [Display(Name = "刊行日")]
     [DataType(DataType.Date)]
     public DateTime Published { get; set; }
+
     [Display(Name = "配布サンプル")]
     public bool Sample { get; set; }
+
     [Timestamp]
     public byte[]? RowVersion { get; set; }
 
