5e219bb79853ec5ea98da68297c77d62a0267192
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-09-08 16:00:01 +0900

    Add Empty Result

--- SelfAspNet/Controllers/ResultController.cs
+++ SelfAspNet/Controllers/ResultController.cs
@@ -52,4 +52,9 @@ public class ResultController : Controller
         }
         return View("../Books/Details", bs);
     }
+
+    public IActionResult Nothing()
+    {
+        return Empty;
+    }
 }
