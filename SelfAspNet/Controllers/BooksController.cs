8a9ee772399fd068c5c100d07767df198b8a740a
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-09-24 13:13:45 +0900

    Supports HTTP methods not supported by browsers

--- SelfAspNet/Controllers/BooksController.cs
+++ SelfAspNet/Controllers/BooksController.cs
@@ -99,7 +99,7 @@ namespace SelfAspNet.Controllers
         // POST: Books/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        [HttpPut]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Isbn,Title,Price,Publisher,Published,Sample,RowVersion")] Book book)
         {
