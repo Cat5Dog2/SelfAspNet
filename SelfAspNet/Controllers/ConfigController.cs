6eb9ea358877469b6c290710c8ff78636e7a442d
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-10-02 12:00:07 +0900

    Add get config info

---
+++ SelfAspNet/Controllers/ConfigController.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelfAspNet.Controllers;
+
+public class ConfigController : Controller
+{
+    private readonly IConfiguration _config;
+    public ConfigController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    public IActionResult Basic()
+    {
+        ViewBag.Published = _config["MyAppOptions:Published"];
+        ViewBag.Project = _config["MyAppOptions:Projects:0"];
+        return View();
+    }
+}
