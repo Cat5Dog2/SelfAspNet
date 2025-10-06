50eb1e6582daff6bb045d1d68ca469cec2ffc324
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-10-06 10:42:50 +0900

    Add Log

---
+++ SelfAspNet/Controllers/LogController.cs
@@ -0,0 +1,25 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SelfAspNet.Controllers;
+
+public class LogController : Controller
+{
+    private readonly ILogger<LogController> _logger;
+    public LogController(ILogger<LogController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Basic()
+    {
+        _logger.LogTrace("トレース");
+        _logger.LogDebug("デバッグ");
+        _logger.LogInformation("情報");
+        _logger.LogWarning("警告");
+        _logger.LogError("エラー");
+        _logger.LogCritical("致命的な問題");
+
+        return Content("ログはコンソールなどから確認してください");
+    }
+}
