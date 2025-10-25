94f76c22079ebb16c741783c544d7a36e95c88fe
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-10-25 18:11:51 +0900

    Internationalization of Controller Classes

--- SelfAspNet/Controllers/I18nController.cs
+++ SelfAspNet/Controllers/I18nController.cs
@@ -1,13 +1,32 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Localization;
+using Microsoft.Extensions.Localization;
 
 namespace SelfAspNet.Controllers;
 
 public class I18nController : Controller
 {
+    private readonly IStringLocalizer<I18nController> _localizer;
+    private readonly IHtmlLocalizer<I18nController> _htmlLocalizer;
+
+    public I18nController(
+        IStringLocalizer<I18nController> loc,
+        IHtmlLocalizer<I18nController> htmlLoc)
+    {
+        _localizer = loc;
+        _htmlLocalizer = htmlLoc;
+    }
+
     public IActionResult Basic()
     {
+        return View();
+    }
+
+    public IActionResult Ctrl()
+    {
+        ViewBag.Night = _localizer["Night"];
+        ViewBag.Time = _htmlLocalizer["Time", DateTime.Now.ToShortTimeString()];
         return View();
     }
 }
