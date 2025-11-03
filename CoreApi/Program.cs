eb8881b4fc5a6f44b1e8e8a13e7a72bb7b093135
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-11-03 13:42:00 +0900

    Add Support XML

--- CoreApi/Program.cs
+++ CoreApi/Program.cs
@@ -11,7 +11,8 @@ builder.Services.AddDbContext<MyContext>(options =>
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+  .AddXmlSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
